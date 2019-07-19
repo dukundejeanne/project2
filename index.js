84cc843bb0223de1cc1486d1ccca13f63cbe4bc7
Jeannne Dukunde <dukundem90@gmail.com>
2019-07-19 15:46:31 +0200

    add date and names

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function formCal(){
     yyyy= day.getFullYear();
     mm=day.getMonth();
     mm=mm+1;
-    console.log(dd);
-    console.log(yyyy);
-    console.log(mm);
+    // console.log(dd);
+    // console.log(yyyy);
+    // console.log(mm);
    
 
     var gender=document.getElementById("gender").value;
@@ -17,7 +17,7 @@ function formCal(){
     var cc= parseFloat(c);
     var day=parseFloat( ( (cc/4) -2*cc-1) + ((5*yyyy/4) ) + ((26*(mm+1)/10)) + dd )%7;
     var day=(day.toFixed(0));
-    console.log(day);
+    // console.log(day);
     
     if(day==0 && gender=='female'){
         document.getElementById('sell').innerHTML='Akosua'  
@@ -38,14 +38,19 @@ else if(day==6 && gender=='female'){
 }
 else if(day==0 && gender=='male'){
     document.getElementById('sell').innerHTML='AKwasi'
-}
-
-
-  
-   
-    
-
-
+}else if(day==1 && gender=='male'){
+    document.getElementById('sell').innerHTML='Kwadwo'
+}else if(day==2 && gender=='male'){
+    document.getElementById('sell').innerHTML='Kwabena'
+}else if(day==3 && gender=='male'){
+    document.getElementById('sell').innerHTML='Kwaku'
+}else if(day==4 && gender=='male'){
+    document.getElementById('sell').innerHTML='Yaw"'
+}else if(day==5 && gender=='male'){
+    document.getElementById('sell').innerHTML='kofi'
+}else if(day==6 && gender=='male'){
+    document.getElementById('sell').innerHTML='Kwame'
+}   
 }
 
 
